78bc1454ffeb91ad7b532630c9b3156ba30a3e7e
Michael-Anderson99 <xmichael299@gmail.com>
2021-02-07 01:00:34 +0000

    The teamDisplay section will now split a full 10 person playerlist based on their position in the loop, and display it on the next screen. it is the most basic form of team picking

--- app/src/main/java/com/example/footyworld/User/TeamDisplay.java
+++ app/src/main/java/com/example/footyworld/User/TeamDisplay.java
@@ -22,7 +22,8 @@ public class TeamDisplay extends AppCompatActivity {
     List<String> team1 = new ArrayList<>();
     List<String> team2 = new ArrayList<>();
     ArrayList<Team> team;
-    String[] arr = new  String[20];
+    String[] arr1 = new  String[20];
+    String[] arr2 = new String[20];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,49 +41,90 @@ public class TeamDisplay extends AppCompatActivity {
         listView1 = findViewById(R.id.team1);
         listView2 = findViewById(R.id.team2);
 
+        //arr1 left side list view
         for(int i=0; i<team.get(0).getAttackers().length; i++)
         {
-             arr = team.get(0).getAttackers();
+             arr1 = team.get(0).getAttackers();
 
-            for(i = 0; i<arr.length; i++)
+            for(i = 0; i<arr1.length; i++)
             {
-                if(arr[i] != null)
+                if(arr1[i] != null)
                 {
-                    team1.add(arr[i]);
+                    team1.add(arr1[i]);
                 }
             }
         }
         for(int i=0; i<team.get(0).getMidfielders().length; i++)
         {
-            arr = team.get(0).getMidfielders();
+            arr1 = team.get(0).getMidfielders();
 
-            for(i = 0; i<arr.length; i++)
+            for(i = 0; i<arr1.length; i++)
             {
-                if(arr[i] != null)
+                if(arr1[i] != null)
                 {
-                    team1.add(arr[i]);
+                    team1.add(arr1[i]);
                 }
             }
         }
         for(int i=0; i<team.get(0).getDefenders().length; i++)
         {
-            arr = team.get(0).getDefenders();
+            arr1 = team.get(0).getDefenders();
 
-            for(i = 0; i<arr.length; i++)
+            for(i = 0; i<arr1.length; i++)
             {
-                if(arr[i] != null)
+                if(arr1[i] != null)
                 {
-                    team1.add(arr[i]);
+                    team1.add(arr1[i]);
+                }
+            }
+        }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //arr2 right side list view
+        for(int i=0; i<team.get(1).getAttackers().length; i++)
+        {
+            arr2 = team.get(1).getAttackers();
+
+            for(i = 0; i<arr2.length; i++)
+            {
+                if(arr2[i] != null)
+                {
+                    team2.add(arr2[i]);
+                }
+            }
+        }
+        for(int i=0; i<team.get(1).getMidfielders().length; i++)
+        {
+            arr2 = team.get(1).getMidfielders();
+
+            for(i = 0; i<arr2.length; i++)
+            {
+                if(arr2[i] != null)
+                {
+                    team2.add(arr2[i]);
+                }
+            }
+        }
+        for(int i=0; i<team.get(1).getDefenders().length; i++)
+        {
+            arr2 = team.get(1).getDefenders();
+
+            for(i = 0; i<arr2.length; i++)
+            {
+                if(arr2[i] != null)
+                {
+                    team2.add(arr2[i]);
                 }
             }
         }
 
 
         ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(TeamDisplay.this, android.R.layout.simple_list_item_1, team1);
-     // ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(TeamDisplay.this, android.R.layout.simple_list_item_1, team2.getDefenders());
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(TeamDisplay.this, android.R.layout.simple_list_item_1, team2);
 
         listView1.setAdapter(adapter1);
-     // listView2.setAdapter(adapter2);
+        listView2.setAdapter(adapter2);
 
 
 
